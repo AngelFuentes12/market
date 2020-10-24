6df9c8ce0dfef1c07e729f308f820779b87d1113
albertoosr <juan.alberto081099@gmail.com>
2020-10-24 18:42:06 -0500

    index

--- views/index.php
+++ views/index.php
@@ -1,8 +1,109 @@
-	<div class="container mt-5 mb-5 p-5">
-		<div class="row">
-			<div class="col-8">
-				<h2>Pagina principal</h2>
-			</div>
-		</div>
-	</div>
+	<section id="index">
+		<div class="container-fluid py-3">
+			<div class="row">
+				<div class="col-6 col-md-2 pb-5 categoria-responsive" style="border-right: 1px solid #f4f4f4;">
+					<small class="h6 pl-3 font-weight-bold">Categorias</small>
+					<div class="dropdown">
+						<ul class="nav flex-column">
+							<li class="nav-item">
+								<!-- <a class="nav-link active" href="#">Active</a> -->
+								<a class="dropdown-toggle nav-link active" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									playeras <i class="far fa-angle-right"></i>
+								</a>
+								<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+									<a class="dropdown-item" href="#">Action</a>
+									<a class="dropdown-item" href="#">Another action</a>
+									<a class="dropdown-item" href="#">Something else here</a>
+								</div>
+							</li>
+							<li class="nav-item">
+								<a class="dropdown-toggle nav-link active" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									playeras <i class="far fa-angle-right"></i>
+								</a>
+								<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+									<a class="dropdown-item" href="#">Action</a>
+									<a class="dropdown-item" href="#">Another action</a>
+									<a class="dropdown-item" href="#">Something else here</a>
+								</div>
+							</li>
+							<li class="nav-item">
+								<a class="dropdown-toggle nav-link active" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									playeras <i class="far fa-angle-right"></i>
+								</a>
+								<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+									<a class="dropdown-item" href="#">Action</a>
+									<a class="dropdown-item" href="#">Another action</a>
+									<a class="dropdown-item" href="#">Something else here</a>
+								</div>
+							</li>
+							<li class="nav-item">
+								<a class="dropdown-toggle nav-link active" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+									playeras <i class="far fa-angle-right"></i>
+								</a>
+								<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
+									<a class="dropdown-item" href="#">Action</a>
+									<a class="dropdown-item" href="#">Another action</a>
+									<a class="dropdown-item" href="#">Something else here</a>
+								</div>
+							</li>
+						</ul>
+					</div>
+				</div>
 
+				<!-- responsive -->
+				<div class="container categoria-movil">
+					<ul class="nav justify-content-center">
+						<li class="nav-item">
+							<a class="nav-link" href="#">electronica</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Jugeteria</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Ropa</a>
+						</li>
+					</ul>
+				</div>
+
+				<div class="col-sm-6 col-md-10">
+					<small class="h6">productos</small>
+					<div class="row">
+						<div class="container">
+							<div class="row">
+								<div class="col-lg-4">
+									<div class="card">
+										<img src="..." class="card-img-top" alt="...">
+										<div class="card-body">
+											<h5 class="card-title">Card title</h5>
+											<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+											<a href="#" class="btn btn-primary">Go somewhere</a>
+										</div>
+									</div>
+								</div>
+								<div class="col-lg-4">
+									<div class="card">
+										<img src="..." class="card-img-top" alt="...">
+										<div class="card-body">
+											<h5 class="card-title">Card title</h5>
+											<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+											<a href="#" class="btn btn-primary">Go somewhere</a>
+										</div>
+									</div>
+								</div>
+								<div class="col-lg-4">
+									<div class="card">
+										<img src="..." class="card-img-top" alt="...">
+										<div class="card-body">
+											<h5 class="card-title">Card title</h5>
+											<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+											<a href="#" class="btn btn-primary">Go somewhere</a>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+
+				</div>
+			</div>
+	</section>
--- views/layouts/user/footer.php
+++ views/layouts/user/footer.php
@@ -1,5 +1,4 @@
-	
-	<div class="container-fluid text-center">
+	<!-- <div class="container-fluid text-center">
 		<div class="row p-3 bg-dark">
 			<div class="col-md-8 mx-auto text-white p-1">
 				<p class="h4">Footer cliente</p>
@@ -11,6 +10,45 @@
     			<p class="h6">Copyright &COPY; 2020 Creativa | All Rights Reserved.</p>
     		</div>
 		</div>
-	</div>
-</body>
-</html>
+	</div> -->
+
+	<section id="footer">
+		<!-- <div class="row ">
+			<div class="col-lg-3">
+				<small class="footer-title">Nosotros</small>
+				<a href="#">paginacion</a>
+				<a href="#">paginacion</a>
+				<a href="#">paginacion</a>
+				<a href="#">paginacion</a>
+			</div>
+			<div class="col-lg-2">
+				<small class="footer-title">Ubicación</small>
+				<a href="#">paginacion</a>
+				<a href="#">paginacion</a>
+				<a href="#">paginacion</a>
+				<a href="#">paginacion</a>
+			</div>
+			<div class="col-lg-3">
+				<small class="footer-title">Atención a clientes</small>
+				<a href="#">paginacion</a>
+				<a href="#">paginacion</a>
+				<a href="#">paginacion</a>
+				<a href="#">paginacion</a>
+			</div>
+			<div class="col-lg-2">
+				<small class="footer-title">Redes sociales</small>
+				<a href="#">paginacion</a>
+				<a href="#">paginacion</a>
+				<a href="#">paginacion</a>
+				<a href="#">paginacion</a>
+			</div>
+		</div> -->
+		</div>
+		<div class="container-fluid copyrigrh">
+			<small class="">© Supermarket 2020 | Todos los Derechos Reservados | <strong>Términos y Condiciones</strong> | <strong> Aviso de Privacidad</strong></small>
+		</div>
+
+	</section>
+	</body>
+
+	</html>
