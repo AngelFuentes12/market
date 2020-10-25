9c77f55838e005279581cacd6af3f0de378f07e7
Angel <marohazel@gmail.com>
2020-10-24 22:08:21 -0500

    Correccion del index

--- views/index.php
+++ views/index.php
@@ -1,57 +1,3 @@
-<<<<<<< HEAD
-	<section id="categorias">
-		<div class="container-fluid">
-			<div class="row">
-				<div class="col-6 col-md-2 py-3">
-					<div class="wrapper">
-						<ul class="list-unstyled components">
-							<h6 class="font-weight-bold">Categorias</h6>
-							<li>
-								<a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Categoria</a>
-								<ul class="collapse list-unstyled" id="homeSubmenu">
-									<li>
-										<a href="#">sucategoria 1</a>
-									</li>
-									<li>
-										<a href="#">sucategoria 2</a>
-									</li>
-									<li>
-										<a href="#">sucategoria 3</a>
-									</li>
-								</ul>
-							</li>
-							<li>
-								<a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Categoria</a>
-								<ul class="collapse list-unstyled" id="pageSubmenu">
-									<li>
-										<a href="#">sucategoria 1</a>
-									</li>
-									<li>
-										<a href="#">sucategoria 2</a>
-									</li>
-									<li>
-										<a href="#">sucategoria 3</a>
-									</li>
-								</ul>
-							</li>
-						</ul>
-					</div>
-				</div>
-
-				<div class="col-sm-6 col-md-10">
-					<small class="h6">productos</small>
-					<div class="row">
-						<div class="container">
-							<div class="row">
-								<div class="col-lg-4">
-									<div class="card">
-										<img src="..." class="card-img-top" alt="...">
-										<div class="card-body">
-											<h5 class="card-title">Card title</h5>
-											<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-											<a href="#" class="btn btn-primary">Go somewhere</a>
-										</div>
-=======
 <section id="categorias">
 	<div class="container-fluid">
 		<div class="row">
@@ -97,8 +43,6 @@
 					</div>
 				</nav>
 			</div>
-
-
 			<div class="col-sm-6 col-md-10">
 				<small class="h6">productos</small>
 				<div class="row">
@@ -111,27 +55,26 @@
 										<h5 class="card-title">Card title</h5>
 										<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
 										<a href="#" class="btn btn-primary">Go somewhere</a>
->>>>>>> 4aaeceea6f1df7f95165dbd03666ed2e699b6515
 									</div>
 								</div>
-								<div class="col-lg-4">
-									<div class="card">
-										<img src="..." class="card-img-top" alt="...">
-										<div class="card-body">
-											<h5 class="card-title">Card title</h5>
-											<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-											<a href="#" class="btn btn-primary">Go somewhere</a>
-										</div>
+							</div>
+							<div class="col-lg-4">
+								<div class="card">
+									<img src="..." class="card-img-top" alt="...">
+									<div class="card-body">
+										<h5 class="card-title">Card title</h5>
+										<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+										<a href="#" class="btn btn-primary">Go somewhere</a>
 									</div>
 								</div>
-								<div class="col-lg-4">
-									<div class="card">
-										<img src="..." class="card-img-top" alt="...">
-										<div class="card-body">
-											<h5 class="card-title">Card title</h5>
-											<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-											<a href="#" class="btn btn-primary">Go somewhere</a>
-										</div>
+							</div>
+							<div class="col-lg-4">
+								<div class="card">
+									<img src="..." class="card-img-top" alt="...">
+									<div class="card-body">
+										<h5 class="card-title">Card title</h5>
+										<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+										<a href="#" class="btn btn-primary">Go somewhere</a>
 									</div>
 								</div>
 							</div>
@@ -140,4 +83,5 @@
 				</div>
 			</div>
 		</div>
-	</section>
+	</div>
+</section>
