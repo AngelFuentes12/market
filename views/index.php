dcaf654dfbdc76af4cc21b89d28a71c5477c4d25
albertoosr <juan.alberto081099@gmail.com>
2020-10-24 19:59:38 -0500

    categorias

--- views/index.php
+++ views/index.php
@@ -1,109 +1,79 @@
-	<section id="index">
-		<div class="container-fluid py-3">
-			<div class="row">
-				<div class="col-6 col-md-2 pb-5 categoria-responsive" style="border-right: 1px solid #f4f4f4;">
-					<small class="h6 pl-3 font-weight-bold">Categorias</small>
-					<div class="dropdown">
-						<ul class="nav flex-column">
-							<li class="nav-item">
-								<!-- <a class="nav-link active" href="#">Active</a> -->
-								<a class="dropdown-toggle nav-link active" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									playeras <i class="far fa-angle-right"></i>
-								</a>
-								<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-									<a class="dropdown-item" href="#">Action</a>
-									<a class="dropdown-item" href="#">Another action</a>
-									<a class="dropdown-item" href="#">Something else here</a>
-								</div>
-							</li>
-							<li class="nav-item">
-								<a class="dropdown-toggle nav-link active" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									playeras <i class="far fa-angle-right"></i>
-								</a>
-								<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-									<a class="dropdown-item" href="#">Action</a>
-									<a class="dropdown-item" href="#">Another action</a>
-									<a class="dropdown-item" href="#">Something else here</a>
-								</div>
-							</li>
-							<li class="nav-item">
-								<a class="dropdown-toggle nav-link active" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									playeras <i class="far fa-angle-right"></i>
-								</a>
-								<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-									<a class="dropdown-item" href="#">Action</a>
-									<a class="dropdown-item" href="#">Another action</a>
-									<a class="dropdown-item" href="#">Something else here</a>
-								</div>
-							</li>
-							<li class="nav-item">
-								<a class="dropdown-toggle nav-link active" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-									playeras <i class="far fa-angle-right"></i>
-								</a>
-								<div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
-									<a class="dropdown-item" href="#">Action</a>
-									<a class="dropdown-item" href="#">Another action</a>
-									<a class="dropdown-item" href="#">Something else here</a>
-								</div>
-							</li>
-						</ul>
-					</div>
-				</div>
-
-				<!-- responsive -->
-				<div class="container categoria-movil">
-					<ul class="nav justify-content-center">
-						<li class="nav-item">
-							<a class="nav-link" href="#">electronica</a>
+<section id="categorias">
+	<div class="container-fluid">
+		<div class="row">
+			<div class="col-6 col-md-2 py-3">
+				<div class="wrapper">
+					<ul class="list-unstyled components">
+						<h6 class="font-weight-bold">Categorias</h6>
+						<li>
+							<a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Categoria</a>
+							<ul class="collapse list-unstyled" id="homeSubmenu">
+								<li>
+									<a href="#">sucategoria 1</a>
+								</li>
+								<li>
+									<a href="#">sucategoria 2</a>
+								</li>
+								<li>
+									<a href="#">sucategoria 3</a>
+								</li>
+							</ul>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Jugeteria</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#">Ropa</a>
+						<li>
+							<a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Categoria</a>
+							<ul class="collapse list-unstyled" id="pageSubmenu">
+								<li>
+									<a href="#">sucategoria 1</a>
+								</li>
+								<li>
+									<a href="#">sucategoria 2</a>
+								</li>
+								<li>
+									<a href="#">sucategoria 3</a>
+								</li>
+							</ul>
 						</li>
 					</ul>
 				</div>
+			</div>
 
-				<div class="col-sm-6 col-md-10">
-					<small class="h6">productos</small>
-					<div class="row">
-						<div class="container">
-							<div class="row">
-								<div class="col-lg-4">
-									<div class="card">
-										<img src="..." class="card-img-top" alt="...">
-										<div class="card-body">
-											<h5 class="card-title">Card title</h5>
-											<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-											<a href="#" class="btn btn-primary">Go somewhere</a>
-										</div>
+			<div class="col-sm-6 col-md-10">
+				<small class="h6">productos</small>
+				<div class="row">
+					<div class="container">
+						<div class="row">
+							<div class="col-lg-4">
+								<div class="card">
+									<img src="..." class="card-img-top" alt="...">
+									<div class="card-body">
+										<h5 class="card-title">Card title</h5>
+										<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+										<a href="#" class="btn btn-primary">Go somewhere</a>
 									</div>
 								</div>
-								<div class="col-lg-4">
-									<div class="card">
-										<img src="..." class="card-img-top" alt="...">
-										<div class="card-body">
-											<h5 class="card-title">Card title</h5>
-											<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-											<a href="#" class="btn btn-primary">Go somewhere</a>
-										</div>
+							</div>
+							<div class="col-lg-4">
+								<div class="card">
+									<img src="..." class="card-img-top" alt="...">
+									<div class="card-body">
+										<h5 class="card-title">Card title</h5>
+										<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+										<a href="#" class="btn btn-primary">Go somewhere</a>
 									</div>
 								</div>
-								<div class="col-lg-4">
-									<div class="card">
-										<img src="..." class="card-img-top" alt="...">
-										<div class="card-body">
-											<h5 class="card-title">Card title</h5>
-											<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-											<a href="#" class="btn btn-primary">Go somewhere</a>
-										</div>
+							</div>
+							<div class="col-lg-4">
+								<div class="card">
+									<img src="..." class="card-img-top" alt="...">
+									<div class="card-body">
+										<h5 class="card-title">Card title</h5>
+										<p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+										<a href="#" class="btn btn-primary">Go somewhere</a>
 									</div>
 								</div>
 							</div>
 						</div>
 					</div>
-
 				</div>
 			</div>
-	</section>
+</section>
