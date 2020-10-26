329f982a4bcafd2e92d00943c8cbcdced0f646b6
vanexo <anvapesi@gmail.com>
2020-10-25 23:44:27 -0600

    Tabulacion de vista slider

--- views/admin/slider/show.php
+++ views/admin/slider/show.php
@@ -1,62 +1,83 @@
 <div id="content" class="container-fluid py-5">
     <section>
+
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-12 col-md-7 form-color p-4 shadow-sm">
+
                     <h4 class="pb-1">Pagína de slider</h4>
                     <form action="" class="py-3">
                         <div class="form-group">
                             <div class="input-group">
+                                
                                 <div class="input-group-prepend">
+
                                     <span class="input-group-text">Subir</span>
                                 </div>
+
                                 <div class="custom-file">
                                     <input type="file" class="custom-file-input">
                                     <label class="custom-file-label text-file" for="files">Elegir <span style="color: red;">*</span></label>
                                 </div>
+
                                 <div class="invalid-feedback">
                                     alert!
                                 </div>
+
                             </div>
                         </div>
                     </form>
+
                 </div>
             </div>
         </div>
+
     </section>
 
+
+
     <section  id="imagen-guardada" class="py-3">
+
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-12 col-md-10 form-color p-4 shadow-sm">
                     <h4 class="pb-1 text-slider font-weight-bold">Imaganes guardadas</h4>
                     <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
                         <div class="carousel-inner">
+
                             <div class="carousel-item active">
                                 <img class="d-block w-100" src="" alt="First slide">
                             </div>
+
                             <div class="carousel-item">
                                 <img class="d-block w-100" src="" alt="Second slide">
                             </div>
+
                             <div class="carousel-item">
                                 <img class="d-block w-100" src="" alt="Third slide">
                             </div>
+
                         </div>
+
                         <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
                             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
                             <span class="sr-only">Previous</span>
                         </a>
+
                         <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
                             <span class="carousel-control-next-icon" aria-hidden="true"></span>
                             <span class="sr-only">Next</span>
                         </a>
+
                     </div>
                 </div>
             </div>
         </div>
+
     </section>
 
     <section id="tabla-imaganes" class="py-3">
+
         <div class="container">
             <div class="row justify-content-center">
                 <div class="col-12 col-md-10 form-color p-4 shadow-sm">
@@ -64,31 +85,42 @@
                     <div class="row mb-3">
                         <div class="col-xl-12 col-lg-12">
                             <div class="table-responsive">
+
                                 <table class="table">
                                     <thead>
                                         <tr>
                                             <th scope="col"><small class="font-weight-bold">ID<small></th>
-                                            <th scope="col"><small class="font-weight-bold">Nombre<small></th>
-                                            <th scope="col"><small class="font-weight-bold">Status<small></th>
-                                            <th scope="col"><small class="font-weight-bold">#<small></th>
-                                            <th scope="col"><small class="font-weight-bold">Eliminar<small></th>
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <tr>
-                                            <td><span class="d-block">1</span><small class="text-muted">imagen.png</small></td>
-                                            <td class="align-middle"><span class="status-span badge-secondary text-secondary">Nombre</span></td>
-                                            <td class="align-middle"><span class="status-span badge-primary badge-active">Activo</span></td>
-                                            <td class="align-middle"><a href="#" class="status-span badge-primary badge-activo"><i class="fas fa-arrow-down"></i></a></td>
-                                            <td class="align-middle"><a href="#" class="status-span badge-primary badge-delete"><i class="fas fa-trash-alt"></i></a></td>
-                                        </tr>
-                                    </tbody>
-                                </table>
+                                                <th scope="col"><small class="font-weight-bold">Nombre<small></th>
+                                                    <th scope="col"><small class="font-weight-bold">Status<small></th>
+                                                        <th scope="col"><small class="font-weight-bold">#<small></th>
+                                                            <th scope="col"><small class="font-weight-bold">Eliminar<small></th>
+                                                            </tr>
+                                                        </thead>
+
+                                                        <tbody>
+                                                            <tr>
+                                                                <td><span class="d-block">1</span><small class="text-muted">imagen.png</small></td>
+
+                                                                <td class="align-middle"><span class="status-span badge-secondary text-secondary">Nombre</span></td>
+
+                                                                <td class="align-middle"><span class="status-span badge-primary badge-active">Activo</span></t
+
+                                                                <td class="align-middle"><a href="#" class="status-span badge-primary badge-activo"><i class="fas fa-arrow-down"></i></a></td>
+
+                                                                <td class="align-middle"><a href="#" class="status-span badge-primary badge-delete"><i class="fas fa-trash-alt"></i></a></td>
+
+                                                            </tr>
+
+                                                        </tbody>
+
+                                                    </table>
+
+
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
+                        </section>
                     </div>
-                </div>
-            </div>
-        </div>
-    </section>
-</div>
